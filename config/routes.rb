94c882eb99cfd7adf16e5abd6d957e8ba793b049
Miryam Bathilde <miryam@leadingdev.com>
2022-10-31 14:12:09 +0100

    URL Helpers and link_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'about', to: 'about#index'
+  get 'about-us', to: 'about#index', as: 'about'
 
   root to: "main#index"
 end
