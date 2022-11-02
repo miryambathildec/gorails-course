dcbf44e7d30608925e3488182ca03fe2d0c16e34
Miryam Bathilde <miryam@leadingdev.com>
2022-11-02 09:44:45 +0100

    handling sign up errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -13,6 +13,7 @@ class RegistrationsController < ApplicationController
   end
 
   private
+  
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@
 
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Must be a valid email address' }
+  validates :password, presence: true, length: { minimum: 6 }
 end
