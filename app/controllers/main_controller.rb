14fdc8989dcd6416570318000b04bcb5763015df
Miryam Bathilde <miryam@leadingdev.com>
2022-11-02 10:12:55 +0100

    Login with Session Cookies

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,8 @@
 class MainController < ApplicationController
   def index
     # This is the default action for the main controller
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    end
   end
 end
